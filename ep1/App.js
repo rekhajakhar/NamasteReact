2c0c864da7d955c70ad0e89f324cdbb2869a73f0
rjakhar <rekha_jakhar@intuit.com>
2026-01-30 17:49:53 +0530

    ep2

--- ep1/App.js
+++ ep1/App.js
@@ -8,20 +8,27 @@
 
 */
 
-const heading = React.createElement("div",{id: "parent"},
-    [React.createElement("div",{id:"child1"},
-        [React.createElement("h1",{id:"h1"},"i am h1 tag"),
-        React.createElement("h2",{id:"h2"},"i am h2 tag")
-        ]
-        ),
-    React.createElement("div",{id:"child1"},
-        [React.createElement("h1",{id:"h1"},"i am h1 tag"),
-        React.createElement("h2",{id:"h2"},"i am h2 tag")
-        ]
-        )
-    ]);
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child1" },
+    React.createElement("h1", { id: "h1-1" }, "i am h1 tag"),
+    React.createElement("h2", { id: "h2-1" }, "i am h2 tag")
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    React.createElement("h1", { id: "h1-2" }, "i am h1 tag"),
+    React.createElement("h2", { id: "h2-2" }, "i am h2 tag")
+  )
+);
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(heading);
