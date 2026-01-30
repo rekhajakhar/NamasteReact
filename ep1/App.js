595e49d6b72e0f9ade80e0c7df1908ef377964a3
rjakhar <rekha_jakhar@intuit.com>
2026-01-30 19:56:36 +0530

    ep3

--- ep1/App.js
+++ ep1/App.js
@@ -11,14 +11,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+/* ep1 code:
 const heading = React.createElement(
   "div",
   { id: "parent" },
   React.createElement(
     "div",
     { id: "child1" },
-    React.createElement("h1", { id: "h1-1" }, "i am h1 tag"),
-    React.createElement("h2", { id: "h2-1" }, "i am h2 tag")
+    React.createElement("h1", { id: "h1-1" }, "React course"),
+    React.createElement("h2", { id: "h2-1" }, "rekha")
   ),
   React.createElement(
     "div",
@@ -27,8 +28,22 @@ const heading = React.createElement(
     React.createElement("h2", { id: "h2-2" }, "i am h2 tag")
   )
 );
+*/
+const HeadingComponent2 = () => {
+    return (<h1 className="heading">Namaste React</h1>)
+} 
+
+const HeadingComponent = () => {
+    return (
+        <div>
+        <HeadingComponent2 />
+        <h1 className="heading"> Namaste React from component</h1>
+        </div>
+    )
+}
+
 
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent/>);
